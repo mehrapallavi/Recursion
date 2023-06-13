53a37e55b2db6ea274f7d7360a1edb64e3f5ba0b
Pallavi Mehra <117376335+mehrapallavi@users.noreply.github.com>
2023-06-13 18:52:52 +0530

    Add files via upload

---
+++ maze2.java
@@ -0,0 +1,20 @@
+public class maze2 {
+
+    static void getMazePos(int currentRow , int currentCol, int endRow, int endCol,String res){
+if(currentCol == endCol && currentRow == endRow){
+System.out.print(res+" ");
+return;
+}
+if(currentCol>endCol || currentRow>endRow){
+return ;
+}
+getMazePos(currentRow, currentCol+1, endRow, endCol,res+"R");
+getMazePos(currentRow+1, currentCol, endRow, endCol,res+"D");
+getMazePos(currentRow+1, currentCol+1, endRow, endCol,res+"I");
+}
+public static void main(String[] args) {
+String result="";
+getMazePos(0, 0, 2, 2,result);
+}
+    
+}
